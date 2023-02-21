2ae02ee54a6c77352d41e071bdf26adb36277057
Suyun-Kim <61129726+Suyun-Kim@users.noreply.github.com>
2023-02-21 12:31:23 +0900

    init

---
+++ src/test/java/com/example/rlotgatewayservice/RlotGatewayServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.rlotgatewayservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RlotGatewayServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
